fb12b13cb1b0060e429a24f6b70530d2948afeca
Pittuk <pittuk@gmail.com>
2023-07-03 22:17:33 -0400

    Carga de archivos

---
+++ src/main/java/com/mycompany/challengeoneconversor/ChallengeOneConversor.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.challengeoneconversor;
+
+import com.mycompany.challengeoneconversor.igu.Pantalla;
+
+/**
+ *
+ * @author pittu
+ */
+public class ChallengeOneConversor {
+
+    public static void main(String[] args) {
+        Pantalla pantalla=new Pantalla();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+    }
+}
